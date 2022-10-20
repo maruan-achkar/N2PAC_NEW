013f7170f74ff3c106d12b13cd3b2da55351e9f2
maruan-achkar <maruan.achkar@catolicasc.edu.br>
2022-10-20 14:52:30 -0300

    more fixes to javascript

--- aula06.js
+++ aula06.js
@@ -1,8 +1,8 @@
 import './styles.css'
 import { setupCounter } from './counter.js'
-import * as THREE from '../three/build/three.module';
-import { GLTFLoader } from '../three/addons/loaders/GLTFLoader.js';
-import { OrbitControls} from '../three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from '../node_modules/three/build/three.module';
+import { GLTFLoader } from '../node_modules/three/addons/loaders/GLTFLoader.js';
+import { OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { Light } from 'three';
 
 //---------------------------------------------------------------!!---------=)
