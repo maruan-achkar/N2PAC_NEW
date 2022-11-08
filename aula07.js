fe77a698344176bc331f22f6229378e3cae3e3b1
maruan-achkar <maruan.achkar@catolicasc.edu.br>
2022-11-08 01:32:56 -0300

    fixes

--- aula07.js
+++ aula07.js
@@ -1,4 +1,4 @@
-import './styles.css'
+
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls} from '/three/examples/jsm/controls/OrbitControls.js';
